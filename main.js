b3742e91b42807b87f7ed1f7a514e55e95adce5c
Clément Corbin <corbin.clement@gmail.com>
2020-04-30 14:22:46 +0200

    fixed typo in module name

--- main.js
+++ main.js
@@ -72,7 +72,7 @@ server.route = {
       videoServer[req.page.searchParams.get("title")] = {};
       //movie = await torrents.search(page.searchParams.get("title"));
       movie = {};
-      movie.torrent = await torrents.stream(page.searchParams.get("title"));
+      movie.torrent = await Torrent.stream(page.searchParams.get("title"));
       videoServer[req.page.searchParams.get("title")] = movie;
       videoServer.makeReady();
     }
