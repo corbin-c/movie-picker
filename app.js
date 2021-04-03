ea7808457af80cb3ce00aa582bb554682226ce3f
Clément Corbin <corbin.clement@gmail.com>
2021-04-03 21:59:30 +0200

    routes

--- app.js
+++ app.js
@@ -2,19 +2,89 @@ const express = require("express");
 const IMDB = require("./imdb.js");
 const app = express();
 const port = process.env.PORT || 3000;
+const path = require("path");
+const dist = path.join(__dirname, "dist");
 
-app.use(express.static("dist"));
+app.use(express.static(dist));
 
-/*
- * Routes: (GET only)
- * search(query) //raw, no filter, movies + persons)
- * getRandomMovie()
- * getTrailer(title)
- * getPerson(name || id)
- * getMovie(title)
- * getMovieList(parameters)
- * enrichData(id)
- */
+app.get("/search/:queryString", async (req, res, next) => {
+  try {
+    res.json(await IMDB.suggestions(req.params.queryString));
+  } catch(e) {
+    next(new Error(e));
+  }
+});
+
+app.get("/randomMovie", async (req, res, next) => {
+  try {
+    res.json(await IMDB.getMovies({
+      sort: "user_rating,desc",
+      count: 1,
+      start: Math.floor(Math.random()*10000)
+    }));
+  } catch(e) {
+    next(new Error(e));
+  }
+});
+
+app.get("/person/:queryString", async (req, res, next) => {
+  try {
+    res.json(await IMDB.getPerson(req.params.queryString));
+  } catch(e) {
+    next(new Error(e));
+  }
+});
+
+app.get("/enrich/:id", async (req, res, next) => {
+  try {
+    res.json(await IMDB.wikiDataSPARQL(req.params.id));
+  } catch(e) {
+    next(new Error(e));
+  }
+  res.json(await IMDB.wikiDataSPARQL(req.params.id));
+});
+
+app.get("/movie/:title", async (req, res, next) => {
+  try {
+    res.json(await IMDB.getMovies({
+      "title": req.params.title
+    }));
+  } catch(e) {
+    next(new Error(e));
+  }
+});
+
+app.get("/trailer/:title/:year", async (req, res, next) => {
+  try {
+    res.json(await IMDB.getTrailer(req.params.title,req.params.year));
+  } catch(e) {
+    next(new Error(e));
+  }
+});
+
+app.post("/movies/", async (req, res, next) => {
+  try {
+    res.json(await IMDB.getMovies(req.body));
+  } catch(e) {
+    next(new Error(e));
+  }
+});
+
+app.get("*", function (req, res, next) {
+  const error = new Error("File not found");
+  error.statusCode = 404;
+  next(error);
+});
+
+app.use(function(err, req, res, next) {
+  if (!err.statusCode) err.statusCode = 500;
+  if (err.statusCode === 404) {
+    return res.status(404).sendFile(path.join(dist, "index.html"));;
+  }
+  return res
+    .status(err.statusCode)
+    .json({ error: err.toString() });
+});
 
 app.listen(port, () => {
   console.log("movie picker started on port "+port);
