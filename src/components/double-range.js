9d38a9ef38c7b50808d1cc023d4987bd11346d99
Clément Corbin <corbin.clement@gmail.com>
2021-04-05 21:25:22 +0200

    send user selected values to parent & react to provided values

--- src/components/double-range.js
+++ src/components/double-range.js
@@ -1,18 +1,42 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import "./double-range.css";
 
-function DoubleRange(limits) {
-  const { start, end } = limits;
-  //~ const end = parseInt((new Date()).getFullYear());
+function DoubleRange(props) {
+  const { start, end, providedValue, onRangeChange } = props;
   const [state,setState] = useState({start, end});
+ 
+  const previousValueRef = useRef(providedValue);
+  const previousValue = previousValueRef.current;
+  const previousStateRef = useRef(state);
+  const previousState = previousStateRef.current;
+  
+  useEffect(() => {
+    if (providedValue.done === false) {
+      if ((providedValue.start !== previousValue.start && providedValue.start !== state.start)
+      || (providedValue.end !== previousValue.end && providedValue.end !== state.end)) {
+        setState(prevState => providedValue);
+        onRangeChange({ ...state, providedValue: true });
+      }
+    }
+    if ((state.start !== previousState.start) || (state.end !== previousState.end)) {
+      onRangeChange(state);
+      previousStateRef.current = state;
+    }
+    if ((providedValue.done !== previousValue.done)
+      || (providedValue.start !== previousValue.start)
+      || (providedValue.end !== previousValue.end)) {
+      previousValueRef.current = providedValue;
+    }
+  });
   const onChange = (e) => {
     setState(prevState => {
       let name = e.target.name;
       let value = parseInt(e.target.value);
+      let state = { ...prevState };
       if (((name === "start") && (value <= state.end)) || (name === "end")) {
-        return ({ ...prevState, [name]: value });
+        state = { ...prevState, [name]: value };
       }
-      return ({ ...prevState });
+      return (state);
     });
   };
   const renderRange = (name, style) => {
@@ -36,8 +60,8 @@ function DoubleRange(limits) {
   let width = "max(" + endValue + "% - " + rem + "rem, 1.26rem)";
   let endStyle = { width };
   let startStyle = {
-    "--wOrZ": "max(" + width + " - 1.26rem, 0rem)",
-  }
+    "--zi": (state.start === end) ? 20:10
+  };
   /* style for indicator */
   let endPosition = 1-((state.end - state.start) / (end - state.start));
   let startPosition = (state.start - start) / (end - start);
@@ -52,13 +76,13 @@ function DoubleRange(limits) {
       + " + " + (endRem-.1) + "rem)",
   }
   return (
-    <fieldset className="flex flex-row justify-evenly">
-      <span className="flex-grow text-center">{ state.start }</span>
-      <div className="w-5/6 relative ranges" style={ indicatorStyle }>
+    <fieldset className="flex flex-wrap md:flex-nowrap flex-row justify-evenly mb-5 md:mb-2">
+      <span className="w-1/2 order-1 md:w-auto flex-grow text-left md:text-center mb-2 md:mb-0 pl-2 md:pl-0">{ state.start }</span>
+      <div className="w-full order-3 md:order-2 md:w-5/6 relative ranges" style={ indicatorStyle }>
         { renderRange("start", startStyle) }
         { renderRange("end", endStyle) }
       </div>
-      <span className="flex-grow text-center">{ state.end }</span>
+      <span className="w-1/2 order-2 md:order-3 md:w-auto flex-grow text-right md:text-center mb-2 md:mb-0 pr-2 md:pr-0">{ state.end }</span>
     </fieldset>
   )
 }
