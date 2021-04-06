2eba4309bd4d85cefa0cae5eb2f9250fe1c4b9e3
Clément Corbin <corbin.clement@gmail.com>
2021-04-06 12:44:44 +0200

    fieldgrid class

--- src/components/date-selector.js
+++ src/components/date-selector.js
@@ -81,6 +81,7 @@ function DateSelector() {
       return state;
     });
   };
+  
   return (
     <section>
       <DoubleRange
@@ -91,7 +92,7 @@ function DateSelector() {
       <fieldset
         ref={ decadeRef }
         onChange={ changeValues }
-        className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5">
+        className="fieldgrid">
         { makeRadios() }
       </fieldset>
     </section>
