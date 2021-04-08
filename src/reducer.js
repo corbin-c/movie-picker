770b33f55983d28bd964cfa5f978bdb24beee944
Clément Corbin <corbin.clement@gmail.com>
2021-04-08 16:39:16 +0200

    using external reducers

--- src/reducer.js
+++ src/reducer.js
@@ -1,58 +1,25 @@
+import filtersReducer from "./reducers/filters.js";
+import moviesReducer from "./reducers/movies.js";
+
 const initialState = {
   filters: {
     dates: { start: 0, end: 0 },
     genres: [],
+    persons: {
+      p0: {name:"", id:""},
+      p1: {name:"", id:""}
+    },
     awards: []
+  },
+  movies: {
+    body: {},
+    results: []
   }
-}
-const filtersReducer = (state, action) => {
-  if (action.type === "filters/dates/set") {
-    return {
-      ...state,
-      dates: action.payload
-    }
-  } else if (action.type === "filters/awards/add") {
-    return {
-      ...state,
-      awards: [...state.awards, action.payload]
-    }
-  } else if (action.type === "filters/awards/remove") {
-    return {
-      ...state,
-      awards: state.awards.filter(e => e !== action.payload)
-    }
-  } else if (action.type === "filters/awards/set") {
-    return {
-      ...state,
-      awards: action.payload
-    }
-  } else if (action.type === "filters/genres/add") {
-    return {
-      ...state,
-      genres: [...state.genres, action.payload]
-    }
-  } else if (action.type === "filters/genres/remove") {
-    return {
-      ...state,
-      genres: state.genres.filter(e => e !== action.payload)
-    }
-  } else if (action.type === "filters/genres/set") {
-    return {
-      ...state,
-      genres: action.payload
-    }
-  }
-  return state;
 }
 
 export default function rootReducer(state = initialState, action) {
-  // always return a new object for the root state
-
   return {
-    filters: filtersReducer(state.filters,action)
+    filters: filtersReducer(state.filters,action),
+    movies: moviesReducer(state.movies, action)
   }
-    // the value of `state.todos` is whatever the todos reducer returns
-    //~ todos: todosReducer(state.todos, action),
-    // For both reducers, we only pass in their slice of the state
-    //~ filters: filtersReducer(state.filters, action)
 }
