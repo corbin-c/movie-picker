467de06d311fc108419c8ad1866367cd4217cec9
Clément Corbin <corbin.clement@gmail.com>
2021-04-09 08:54:44 +0200

    scale -1

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,9 @@ module.exports = {
  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    scale: {
+      "x": "-1",
+    },
     extend: {},
   },
   variants: {
