e8d1c73c9455301f1102de38729ae0a9fa998fb3
Clément Corbin <corbin.clement@gmail.com>
2021-04-12 22:44:32 +0200

    progressive loading

--- src/components/movie-poster.js
+++ src/components/movie-poster.js
@@ -1,22 +1,44 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-/* this displays a movie poster with srcset if available */
+/* this displays a movie poster */
 
 function MoviePoster(props) {
-  const { cover, title } = props;
+  const { cover, title, classes, size, vw, type } = props;
+  const [blur,setBlur] = useState(" blur");
+  const [ready,setReady] = useState(false);
+  useEffect(() => {
+    if (!cover) {
+      return;
+    }
+    setReady(state => false);
+    setBlur(state => " blur");
+    if ((size !== "small") && (vw > 320) && (cover.small)) {
+      let target = new Image();
+      target.onload = () => {
+        setReady(state => true);
+        setBlur(blur => "");
+      };
+      target.src = cover[size];
+    } else {
+      setBlur(blur => "");
+    }
+  },[cover, size, vw]);
+  if ((!cover) || (Object.keys(cover).length === 0)) {
+    return null;
+  }
   if (cover.small) {
     return (<img
+      className={ (classes || "") + blur }
+      src={ (ready) ? cover[size]:cover.small }
       loading="lazy"
-      srcSet={ [cover.small+" 80w", cover.medium+" 400w"].join(", ") }
-      sizes={ "(max-width: 320px) 80px, 400px" }
-      src={ cover.big }
-      alt={ "Movie poster for '" + title + "'" } />)
+      alt={ ((type) ? "Picture of ":"Movie poster for ") + title } />
+    );
   }
   return (<img
+    className={ (classes || "") }
     loading="lazy"
     src={ cover.big }
-    alt={ "Movie poster for '" + title + "'" } />
-  )
+    alt={ ((type) ? "Picture of ":"Movie poster for ")+ title } />)
 }
 
 export default MoviePoster;
