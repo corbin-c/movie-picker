dbbe330cdaa13c316640afbcac5dfc2b9b903398
Clément Corbin <corbin.clement@gmail.com>
2021-04-13 12:23:36 +0200

    organizing components

--- src/components/item-view/back-button.js
+++ src/components/item-view/back-button.js
@@ -12,8 +12,8 @@ function BackButton(props) {
   };
   return (<a onClick={ handleClick } title={ title } className="btn-icon fixed top-2 left-2" href="/">
       { (icon === "arrow")
-        ? <svg xmlns="http://www.w3.org/2000/svg" className="fill-current" viewBox="0 0 24 24" width="24" height="24"><path fillRule="evenodd" d="M10.78 19.03a.75.75 0 01-1.06 0l-6.25-6.25a.75.75 0 010-1.06l6.25-6.25a.75.75 0 111.06 1.06L5.81 11.5h14.44a.75.75 0 010 1.5H5.81l4.97 4.97a.75.75 0 010 1.06z"></path></svg>
-        : <svg width="24px" height="24px" xmlns="http://www.w3.org/2000/svg" className="fill-current">
+        ? <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path fillRule="evenodd" d="M10.78 19.03a.75.75 0 01-1.06 0l-6.25-6.25a.75.75 0 010-1.06l6.25-6.25a.75.75 0 111.06 1.06L5.81 11.5h14.44a.75.75 0 010 1.5H5.81l4.97 4.97a.75.75 0 010 1.06z"></path></svg>
+        : <svg width="24px" height="24px" xmlns="http://www.w3.org/2000/svg">
             <defs>
               <svg id="square" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" width="16" height="16"><path fillRule="evenodd" d="M4 5.75C4 4.784 4.784 4 5.75 4h4.5c.966 0 1.75.784 1.75 1.75v4.5A1.75 1.75 0 0110.25 12h-4.5A1.75 1.75 0 014 10.25v-4.5zm1.75-.25a.25.25 0 00-.25.25v4.5c0 .138.112.25.25.25h4.5a.25.25 0 00.25-.25v-4.5a.25.25 0 00-.25-.25h-4.5z"></path></svg>
             </defs>
--- src/components/item-view/trailer.js
+++ src/components/item-view/trailer.js
@@ -16,7 +16,7 @@ function TrailerOverlay(props) {
   return (
     <div ref={ div } onKeyDown={ tryToExit } tabIndex="1" className={ classes + "trailer fixed top-0 left-0 w-screen h-screen bg-opacity-80 bg-black flex justify-center items-center"}>
       <button title="Close trailer" className="btn-icon absolute top-2 right-2" onClick={ handleChange }>
-        <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path fillRule="evenodd" d="M5.72 5.72a.75.75 0 011.06 0L12 10.94l5.22-5.22a.75.75 0 111.06 1.06L13.06 12l5.22 5.22a.75.75 0 11-1.06 1.06L12 13.06l-5.22 5.22a.75.75 0 01-1.06-1.06L10.94 12 5.72 6.78a.75.75 0 010-1.06z"></path></svg>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path fillRule="evenodd" d="M5.72 5.72a.75.75 0 011.06 0L12 10.94l5.22-5.22a.75.75 0 111.06 1.06L13.06 12l5.22 5.22a.75.75 0 11-1.06 1.06L12 13.06l-5.22 5.22a.75.75 0 01-1.06-1.06L10.94 12 5.72 6.78a.75.75 0 010-1.06z"></path></svg>
       </button>
       { ((trailer) && (trailer.youtube) && (classes === ""))
       && <div className="relative video-container">
