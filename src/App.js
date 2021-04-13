a60da17299fc68bcc5a2ca4d8032d706f602bff8
Clément Corbin <corbin.clement@gmail.com>
2021-04-13 15:57:59 +0200

    footer added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
-import store from "./store.js";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Footer from "./components/footer.js";
 import GridView from "./views/grid-view.js";
 import MovieView from "./views/movie-view.js";
 import PersonView from "./views/person-view.js";
-import "./App.css";
 
 function App() {
   return (
@@ -19,17 +18,8 @@ function App() {
             <PersonView />
           </Route>
         </Switch>
+        <Footer />
     </Router>
   );
 }
 export default App;
-
-/*
- * Disclaimer:
- * This website exposes data from the Internet Movie DataBase (IMDB). All our bases are belong to them.
- * It does not intent to become a competitor of IMDB nor has commercial purpose: It only has experimental
- * and learning purpose.
- * This website basically acts like a proxy to provide a better browsing & film discovery experience and does not store IMDB
- * or user data in any way.
- * All displayed information remains the intellectual property of IMDB or their legal owner..
- */
