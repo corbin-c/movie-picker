086f51c1b7c100ae1aecde0f6a3ced3607061106
Clément Corbin <corbin.clement@gmail.com>
2021-04-13 16:01:24 +0200

    quotes

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux'
-import './index.css';
-import store from './store.js'
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux"
+import "./index.css";
+import store from "./store.js"
+import App from "./App";
 
 
 ReactDOM.render(
@@ -13,10 +12,5 @@ ReactDOM.render(
       <App />
     </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
