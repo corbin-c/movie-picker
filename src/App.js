fbe6886f83792ecf273dd4c15a9591e76cefa843
Clément Corbin <corbin.clement@gmail.com>
2021-04-15 09:24:51 +0200

    home view & 404

--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Footer from "./components/footer.js";
+import HomeView from "./views/home-view.js";
 import GridView from "./views/grid-view.js";
 import MovieView from "./views/movie-view.js";
 import PersonView from "./views/person-view.js";
+import Error404 from "./views/error-404.js";
 
 function App() {
   return (
     <Router>
         <Switch>
-          <Route exact path="/">
-            <GridView />
-          </Route>
           <Route path="/movie/:id">
             <MovieView />
           </Route>
           <Route path="/person/:id">
             <PersonView />
+          </Route>
+          <Route path="/404">
+            <Error404 />
+          </Route>
+          <Route path="/:home?">
+            <HomeView />
+            <GridView />
           </Route>
         </Switch>
         <Footer />
