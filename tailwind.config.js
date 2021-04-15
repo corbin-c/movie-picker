6b84b23818e74b0209232fb8bd52c232c6ca9060
Clément Corbin <corbin.clement@gmail.com>
2021-04-15 11:53:37 +0200

    no zIndex conf

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,16 +2,6 @@ module.exports = {
  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: false, // or "media" or "class"
   theme: {
-    zIndex: {
-      "0": 0,
-      "10": 10,
-      "20": 20,
-      "30": 30,
-      "40": 40,
-      "50": 50,
-      "100": 100,
-      "auto": "auto"
-    },
     scale: {
       "x": "-1",
     },
