e49d02c1d5a37e8c465a9d5b53f797a6ee897830
Clément Corbin <corbin.clement@gmail.com>
2021-04-15 09:34:57 +0200

    extend z-index

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,20 @@
 module.exports = {
  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
-  darkMode: false, // or 'media' or 'class'
+  darkMode: false, // or "media" or "class"
   theme: {
+    zIndex: {
+      "0": 0,
+      "10": 10,
+      "20": 20,
+      "30": 30,
+      "40": 40,
+      "50": 50,
+      "25": 25,
+      "50": 50,
+      "75": 75,
+      "100": 100,
+      "auto": "auto"
+    },
     scale: {
       "x": "-1",
     },
